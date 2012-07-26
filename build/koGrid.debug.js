ac8508be16fd96b561c2e6cff16ddde876f3e1d0
Stephen Commisso <scommisso@godaddy.com>
2012-07-26 09:33:58 -0700

    Fixed formatting in build outputs

--- build/koGrid.debug.js
+++ build/koGrid.debug.js
@@ -2,7 +2,7 @@
 * KoGrid JavaScript Library 
 * (c) Eric M. Barnard 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php) 
-* Compiled At:  9:30:44.61 Thu 07/26/2012 
+* Compiled At:  9:33:18.77 Thu 07/26/2012 
 ***********************************************/ 
 (function(window, undefined){ 
  
