8decb8405209d9c3531f2ac31852f65c80fa5949
Timothy Sweet <timothyswt@gmail.com>
2012-08-27 14:56:37 -0700

    footer template had duplicated code.
    just removing the duplicate code.

--- sKoGrid.debug.js
+++ sKoGrid.debug.js
@@ -1,7 +1,7 @@
 /*********************************************** 
 * sKoGrid JavaScript Library 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php) 
-* Compiled At: 12:55:28.15 Mon 08/27/2012 
+* Compiled At: 14:55:36.71 Mon 08/27/2012 
 ***********************************************/ 
 (function(window, undefined){ 
  
@@ -328,33 +328,6 @@ kg.utils = utils;
                         '</select>' +
                     '</div>' +
                     '<div class="kgPagerControl">' +
-                        '<input class="kgPagerFirst" type="button" data-bind="click: pageToFirst, enable: canPageBackward" title="First Page"/>' +
-                        '<input class="kgPagerPrev" type="button"  data-bind="click: pageBackward, enable: canPageBackward" title="Previous Page"/>' +
-                        '<input class="kgPagerCurrent" type="text" data-bind="value: protectedCurrentPage, enable: maxPages() > 1" />' +
-                        '<input class="kgPagerNext" type="button"  data-bind="click: pageForward, enable: canPageForward" title="Next Page"/>' +
-                        '<input class="kgPagerLast" type="button"  data-bind="click: pageToLast, enable: canPageForward" title="Last Page"/>' +
-                    '</div>' +
-                '</div>' +
-            '</div>';
-};
-
-﻿kg.templates.defaultFooterTemplate = function () {
-    return '<div class="kgTotalSelectContainer" data-bind="visible: footerVisible">' +
-                '<div class="kgFooterTotalItems">' +
-                    '<span class="kgLabel">Total Items:</span> <span data-bind="text: maxRows"></span>' +
-                '</div>' +
-                '<div class="kgFooterSelectedItems">' +
-                    '<span class="kgLabel">Selected Items:</span> <span data-bind="text: selectedItemCount"></span>' +
-                '</div>' +
-            '</div>' +
-            '<div class="kgPagerContainer" data-bind="visible: pagerVisible() && footerVisible()">' +
-                '<div style="float: right;">' +
-                    '<div class="kgRowCountPicker"">' +
-                        '<span class="kgLabel">Rows:</span>' +
-                        '<select data-bind="options: pageSizes, value: selectedPageSize">' +
-                        '</select>' +
-                    '</div>' +
-                    '<div class="kgPagerControl" style="float: left; min-width: 135px;">' +
                         '<input class="kgPagerFirst" type="button" data-bind="click: pageToFirst, enable: canPageBackward" title="First Page"/>' +
                         '<input class="kgPagerPrev" type="button"  data-bind="click: pageBackward, enable: canPageBackward" title="Previous Page"/>' +
                         '<input class="kgPagerCurrent" type="text" data-bind="value: protectedCurrentPage, enable: maxPages() > 1" />' +
--- src/Templates/FooterTemplate.js
+++ src/Templates/FooterTemplate.js
@@ -24,30 +24,3 @@
                 '</div>' +
             '</div>';
 };
-
-﻿kg.templates.defaultFooterTemplate = function () {
-    return '<div class="kgTotalSelectContainer" data-bind="visible: footerVisible">' +
-                '<div class="kgFooterTotalItems">' +
-                    '<span class="kgLabel">Total Items:</span> <span data-bind="text: maxRows"></span>' +
-                '</div>' +
-                '<div class="kgFooterSelectedItems">' +
-                    '<span class="kgLabel">Selected Items:</span> <span data-bind="text: selectedItemCount"></span>' +
-                '</div>' +
-            '</div>' +
-            '<div class="kgPagerContainer" data-bind="visible: pagerVisible() && footerVisible()">' +
-                '<div style="float: right;">' +
-                    '<div class="kgRowCountPicker"">' +
-                        '<span class="kgLabel">Rows:</span>' +
-                        '<select data-bind="options: pageSizes, value: selectedPageSize">' +
-                        '</select>' +
-                    '</div>' +
-                    '<div class="kgPagerControl" style="float: left; min-width: 135px;">' +
-                        '<input class="kgPagerFirst" type="button" data-bind="click: pageToFirst, enable: canPageBackward" title="First Page"/>' +
-                        '<input class="kgPagerPrev" type="button"  data-bind="click: pageBackward, enable: canPageBackward" title="Previous Page"/>' +
-                        '<input class="kgPagerCurrent" type="text" data-bind="value: protectedCurrentPage, enable: maxPages() > 1" />' +
-                        '<input class="kgPagerNext" type="button"  data-bind="click: pageForward, enable: canPageForward" title="Next Page"/>' +
-                        '<input class="kgPagerLast" type="button"  data-bind="click: pageToLast, enable: canPageForward" title="Last Page"/>' +
-                    '</div>' +
-                '</div>' +
-            '</div>';
-};
