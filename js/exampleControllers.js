4869c3a8e25939f6ae6f530211f9cc5aa5ed4f92
jonricaurte <jonricaurte@gmail.com>
2012-12-11 13:02:25 -0800

    Added jQuery Themes example

--- js/exampleControllers.js
+++ js/exampleControllers.js
@@ -61,14 +61,7 @@ angular.module('myApp.exampleControllers', [])
 		$scope.switchTab = function(tab){
 			$scope.selectedBasicTab = $scope.basicTabs[tab];
 		};				
-		$scope.angularBindings = "{{row.getProperty(col.field)}}";
 		$scope.link = function(){ return "partials/exampleDefinitions/templates/cellTemplates/stringCellTemplate/stringCellTemplate" + $scope.selectedBasicTab.title + '.html';};
-		$scope.myData = gridData;
-		$scope.gridOptions = { 
-			data: 'myData',
-			columnDefs: [{field: 'name', displayName: 'Name'},
-						 {field:'age', displayName:'Age', cellTemplate: '<div ng-class="{green: row.getProperty(col.field) > 30}"><div class="ngCellText">{{row.getProperty(col.field)}}</div></div>'}]
-		};
 	}])
 	
 .controller('FileReferencedCellTemplateCtrl', ['$scope', function($scope) {
@@ -78,12 +71,6 @@ angular.module('myApp.exampleControllers', [])
 			$scope.selectedBasicTab = $scope.basicTabs[tab];
 		};				
 		$scope.link = function(){ return "partials/exampleDefinitions/templates/cellTemplates/fileReferencedCellTemplate/fileReferencedCellTemplate" + $scope.selectedBasicTab.title + '.html';};
-		$scope.myData = gridData;
-		$scope.gridOptions = { 
-			data: 'myData',
-			columnDefs: [{field: 'name', displayName: 'Name'},
-						 {field:'age', displayName:'Age', cellTemplate: 'partials/exampleDefinitions/templates/cellTemplates/fileReferencedCellTemplate/cellTemplate.html'}]
-		};
 	}])
 	
 .controller('RowTemplateCtrl', ['$scope', function($scope) {
@@ -92,18 +79,18 @@ angular.module('myApp.exampleControllers', [])
 		$scope.switchTab = function(tab){
 			$scope.selectedBasicTab = $scope.basicTabs[tab];
 		};				
-		$scope.angularBindingsIndex = "{{$index}}";
-		$scope.angularBindingsCellClass = "{{col.cellClass}}";
 		$scope.link = function(){ return "partials/exampleDefinitions/templates/rowTemplates/rowTemplate" + $scope.selectedBasicTab.title + '.html';};
-		$scope.myData = gridData;
-		$scope.gridOptions = { 
-			data: 'myData',
-			rowTemplate: '<div ng-repeat="col in visibleColumns()" class="ngCell col{{$index}} {{col.cellClass}}" ng-class="{\'ui-widget-content\':jqueryUITheme, green: row.getProperty(\'age\')  < 30}" ng-cell></div>'
-		};
+	}])
+		
+.controller('ThemesExampleCtrl', ['$scope', function($scope) {
+		$scope.basicTabs = tabs;
+		$scope.selectedBasicTab = $scope.basicTabs[0];		
+		$scope.switchTab = function(tab){
+			$scope.selectedBasicTab = $scope.basicTabs[tab];
+		};				
+		$scope.link = function(){ return "partials/exampleDefinitions/themes/themes" + $scope.selectedBasicTab.title + '.html';};
 	}])
 	
-	
-
 .controller('PagingExampleCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.basicTabs = tabs;
 	$scope.selectedBasicTab = $scope.basicTabs[0];		
@@ -111,59 +98,6 @@ angular.module('myApp.exampleControllers', [])
 		$scope.selectedBasicTab = $scope.basicTabs[tab];
 	};				
 	$scope.link = function(){ return "partials/exampleDefinitions/paging/paging" + $scope.selectedBasicTab.title + '.html';};
-
-	$scope.filterOptions = {
-        filterText: "",
-        useExternalFilter: true
-    };
-    $scope.pagingOptions = {
-        pageSizes: [250, 500, 1000],
-        pageSize: 250,
-        totalServerItems: 0,
-        currentPage: 1
-    };
-	$scope.setPagingData = function(data, page, pageSize){	
-		var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
-		$scope.myData = pagedData;
-		$scope.pagingOptions.totalServerItems = data.length;
-		if (!$scope.$$phase) {
-			$scope.$apply();
-		}
-	};
-    $scope.getPagedDataAsync = function (pageSize, page, searchText) {
-        setTimeout(function () {
-            var data;
-            if (searchText) {
-                var ft = searchText.toLowerCase();
-				$http.get('jsonFiles/largeLoad.json').success(function (largeLoad) {		
-					data = largeLoad.filter(function(item) {
-						return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
-					});
-					$scope.setPagingData(data,page,pageSize);
-				});            
-            } else {
-				$http.get('jsonFiles/largeLoad.json').success(function (largeLoad) {
-					$scope.setPagingData(largeLoad,page,pageSize);
-				});
-            }
-        }, 100);
-    };
-	
-    $scope.myData = $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
-	
-    $scope.$watch('pagingOptions', function () {
-        $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
-    }, true);
-    $scope.$watch('filterOptions', function () {
-        $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
-    }, true);   
-	
-    $scope.gridOptions = {
-		data: 'myData',
-        enablePaging: true,
-        pagingOptions: $scope.pagingOptions,
-        filterOptions: $scope.filterOptions
-    };	
 }])
 	
 .controller('MasterDetailExampleCtrl', ['$scope', function($scope) {
@@ -174,11 +108,4 @@ angular.module('myApp.exampleControllers', [])
 	};				
 	$scope.angularBindings = "{{mySelections}}";
 	$scope.link = function(){ return "partials/exampleDefinitions/masterDetail/masterDetails" + $scope.selectedBasicTab.title + '.html';};	
-    $scope.mySelections = [];
-	$scope.myData = gridData;
-	$scope.gridOptions = { 
-		data: 'myData',	
-        selectedItems: $scope.mySelections,
-		multiSelect: false
-	};
 }]);
--- js/viewModels.js
+++ js/viewModels.js
@@ -96,6 +96,19 @@ function rowTemplateVM() {
     };
 }
 
+function themesVM(){
+	var self = this;
+	this.myData = ko.observableArray([{name: "Moroni", age: 50},
+                     {name: "Tiancum", age: 43},
+                     {name: "Jacob", age: 27},
+                     {name: "Nephi", age: 29},
+                     {name: "Enos", age: 34}]);
+	this.gridOptions = {
+		data: self.myData,
+		jqueryUITheme: true
+	};
+}
+
 function pagingVm(){
     var self = this;
   
