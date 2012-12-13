96405b087589c32d9b90dbaf803bf4e7d10f1d94
Timothy Sweet <timothyswt@gmail.com>
2012-12-12 21:14:53 -0800

    updating readme for prep

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 ﻿#!/bin/bash
 CurrentDir=$PWD
 OutPutFile=$CurrentDir/koGrid.debug.js
-FinalFile=../koGrid-2.0.7.debug.js
+FinalFile=../koGrid-2.1.0.debug.js
 BuildOrder=$CurrentDir/build-order.txt
 
 echo JSBuild Starting...
--- src/bindingHandlers/ko-grid.js
+++ src/bindingHandlers/ko-grid.js
@@ -24,8 +24,7 @@
             }
             //set the right styling on the container
             elem.addClass("koGrid").addClass(grid.gridId.toString());
-            //call update on the grid, which will refresh the dome measurements asynchronously
-            elem.append(gridElem);// make sure that if any of these change, we re-fire the calc logic
+            elem.append(gridElem);
             grid.$userViewModel = bindingContext.$data;
             ko.applyBindings(grid, gridElem[0]);
             //walk the element's graph and the correct properties on the grid
