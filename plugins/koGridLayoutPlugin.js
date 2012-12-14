4e0b67356098bd96a5e272d4d29fe450f6d5d3f8
Tim Sweet <timothyswt@gmail.com>
2012-12-14 11:22:21 -0800

    Update plugins/koGridLayoutPlugin.js
    replacing tabs with spaces

--- plugins/koGridLayoutPlugin.js
+++ plugins/koGridLayoutPlugin.js
@@ -1,10 +1,10 @@
 koGridLayoutPlugin = function () {
-	var self = this;
-	this.grid = null;
+    var self = this;
+    this.grid = null;
     // The init method gets called during the koGrid binding handler execution.
     self.onGridInit = function (grid) {
         /* logic */
-		self.grid = grid;
+        self.grid = grid;
     };
     this.updateGridLayout = function(){
         kg.domUtilityService.UpdateGridLayout(self.grid);
